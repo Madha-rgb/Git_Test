9040a6747562f54ad95287fcdc86c69abe4efadf
Madha-rgb <amanecer.abril18@gmail.com>
2023-10-02 14:23:08 +0200

    a
    a

--- clase_repaso/Assets/movement.cs
+++ clase_repaso/Assets/movement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    private Rigidbody2D rb;
- 
+    Rigidbody2D rb;
+
+    [SerializeField] int movementSpeed;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -13,13 +15,13 @@ public class movement : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D)) //"if" does not need { } if there is only one line of code for it. I have removed them now.
+        {
+            rb.AddForce(Vector2.right * movementSpeed);
+        }
+        //when adding "new Vector", the "new" specifies that this is a child of Vector 2 as a whole. It's the same as creating
+        //an object in Unity, but in code and for Vector.
+        //ATTENTION: this is the example I was talking about. "rb.AddForce(new Vector2 = (3.0));"
     }
-
-    private void FixedUpdate() //
-    {
-        
-    }
-
 
 }
